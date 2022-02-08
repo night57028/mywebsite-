bf72a96ebb59d2d4bba885cab631795e303f33b0
night57028 <97937646+night57028@users.noreply.github.com>
2022-02-08 21:58:41 +0700

    Create P.php

---
+++ P.php
@@ -0,0 +1,53 @@
+<html>
+  <head>
+    <title>ตารางของฉัน</title>
+  </head>
+  <body>
+    <table border="1">
+      <tr>
+        <th>ลำดับ</th>
+        <th>ชื่อ - นามสกุล</th>
+        <th>ชื่อเล่น</th>
+        <th>รูป</th>
+        <th>ทวิตเตอร์</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>kim jisoo</td>
+        <td>jisoo</td>
+        <td><img src="https://i.pinimg.com/originals/c5/0a/46/c50a46f12c99102d113ed344fa6390d0.jpg" width="100px"></td>
+        <td><a href="https://twitter.com/BLACKPINK">twitter</a></td>
+        <tr>
+        <td>2</td>
+        <td>kim jennie</td>
+        <td>jennie</td>
+        <td><img src="https://i.pinimg.com/736x/7d/e1/0b/7de10be8df846f5531abcceeb448ef8c.jpg" width="100px"></td>
+        <td><a href="https://twitter.com/BLACKPINK">twitter</a></td>
+        <tr>
+        <td>3</td>
+        <td>park chaeyoung</td>
+        <td>rosé</td>
+        <td><img src="https://www.brighttv.co.th/wp-content/uploads/2021/03/rose-blackpink-thanks-fans%E0%B8%9B%E0%B8%81.png" width="100px"></td>
+        <td><a href="https://twitter.com/BLACKPINK">twitter</a></td>
+        <tr>
+        <td>4</td>
+        <td>lalisa manobal</td>
+        <td>lisa</td>
+        <td><img src="https://s359.kapook.com/pagebuilder/a842997f-38e7-4be4-aa8c-eedfc2efd45c.jpg" width="100px"></td>
+        <td><a href="https://twitter.com/BLACKPINK">twitter</a></td>
+        <tr>
+        <td>5</td>
+        <td>อี จี อึน</td>
+        <td>IU</td>
+        <td><img src="https://obs.line-scdn.net/0hsd3KKrn1LEsOMgSTeJxTHDRkLyQ9Xj9IagR9SFJccn93UTwbYQNrfiI0cH4mA2sVYAZlLC0xN3pwVThNMQBr/w644" width="100px"></td>
+        <td><a href="https://twitter.com/_iuofficial">twitter</a></td>
+        <tr>
+        <td>6</td>
+        <td>bea joo hyun</td>
+        <td>irene</td>
+        <td><img src="https://i2.wp.com/www.korseries.com/wp-content/uploads/2020/10/irene.jpeg?fit=1441%2C961&ssl=1" width="100px"></td>
+        <td><a href="https://twitter.com/rvsmtown">twitter</a></td>
+      </tr>
+    </table>
+  </body> 
+</html>
