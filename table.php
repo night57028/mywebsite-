343a404bf6fd0e7dd52c5bd6195a294ba4e183b4
ParitaTapa <97943410+ParitaTapa@users.noreply.github.com>
2022-02-08 14:23:08 +0700

    Update table.php

--- table.php
+++ table.php
@@ -1,1 +1,14 @@
+<html>
+  <head>
+    <title>ตารางของฉัน</title>
+  </head>
+  <body>
+    <table border="1">
+      <tr>
+        <td>คอลัมน์ที่ 1</td>
+        <td>คอลัมน์ที่ 2</td>
+      </tr>
+    </table>
+  </body> 
+</html> 
 
